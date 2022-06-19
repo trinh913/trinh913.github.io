22b01dcba63dc722848af6c872a526c6c3a239a7
trinh913 <truongdangtrinh1@gmail.com>
2022-06-19 15:51:28 +0700

    upload

---
+++ list-music.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+      name: "Đảo không người",
+      artist: "Ngô Anh Đạt",
+      img: "img-1",
+      src : "music-1"
+    },
+    {
+      name: "Đại diện cho trái tim",
+      artist: "Southalid",
+      img: "img-2",
+      src: "music-2"
+    },
+    {
+      name: "Chúng ta làm bạn được không ?",
+      artist: "Thiều Bảo Trâm",
+      img: "img-3",
+      src: "music-3"
+    },
+    {
+      name: "Như anh đã thấy em",
+      artist: "PhucXP",
+      img: "img-4",
+      src: "music-4"
+    },
+    {
+      name: "Diễn viên",
+      artist: "Ngô Anh Đạt",
+      img: "img-5",
+      src: "music-5"
+    },
+    {
+      name: "Có hẹn với thanh xuân",
+      artist: "Simon C x Wuy",
+      img: "img-6",
+      src: "music-6"
+    },
+];
